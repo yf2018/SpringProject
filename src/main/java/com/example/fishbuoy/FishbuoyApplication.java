afd3135ccb62dc2a8f9a316ed4f788c54b839705
Yang Fei <1328439793@qq.com>
2020-08-06 21:53:53 +0800

    Initial commit

---
+++ src/main/java/com/example/fishbuoy/FishbuoyApplication.java
@@ -0,0 +1,13 @@
+package com.example.fishbuoy;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication() //来标注一个主程序类，说明这是一个spring boot应用
+public class FishbuoyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FishbuoyApplication.class, args);
+
+    }
+}
+
---
+++ src/main/java/com/example/fishbuoy/repository/AqiRepository.java
@@ -0,0 +1,7 @@
+package com.example.fishbuoy.repository;
+import com.example.fishbuoy.entity.Aqi;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AqiRepository extends JpaRepository<Aqi,String> {
+
+}
